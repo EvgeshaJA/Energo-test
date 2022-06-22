bce97fead125e14115f00cf3edb03b5a5f84789a
Евгения <69339596+EvgeshaJA@users.noreply.github.com>
2022-06-22 21:33:40 +0300

    Add files via upload

---
+++ assets/php/send.php
@@ -0,0 +1,63 @@
+<?php
+// Файлы phpmailer
+require './PHPMailer.php';
+require './SMTP.php';
+require './Exception.php';
+
+// Переменные, которые отправляет пользователь, они равны инпутам из формы
+$name = $_POST['name'];
+$email = $_POST['phone'];
+$messege = $_POST['messege'];
+
+
+
+// Формирование самого письма
+$title = "Заголовок письма";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Почта:</b> $email<br><br>
+<b>Вопрос:</b> $messege<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'atmosferavoda@mail.ru'; // Логин на почте
+    $mail->Password   = 'password'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('atmosferavoda@mail.ru', 'Заявка'); // Адрес самой почты (сайта) и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('atmosferavoda@mail.ru'); //Ваш email
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";}
+else {
+echo 'error'
+$result = "error";
+}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+
+?>
